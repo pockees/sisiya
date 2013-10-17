2cb759a12038685204c28336cd741aaff3b702f7
Erdal Mutlu <erdal@sisiya.net>
2013-10-18 00:42:14 +0300

    rewriting check scripts in Perl

--- sisiya_check_perl/special/sisiya_oracle_hitratios.pl
+++ sisiya_check_perl/special/sisiya_oracle_hitratios.pl
@@ -32,8 +32,21 @@ if(-f $SisIYA_Config::sisiya_functions) {
 #######################################################################################
 ###############################################################################
 #### the default values
-our $ps_prog = 'ps';
-our @progs;
+our $sqlplus_prog = 'sqlplus';
+our %hitratios = ( 
+		'buffer_cache' 	=> { 'warning' => 95, 'error' => 90 },
+		'buffer_pool' 	=> { 'warning' => 95, 'error' => 90 },
+		'dictionary'	=> { 'warning' => 95, 'error' => 90 },
+		'library'	=> { 'warning' => 95, 'error' => 90 },
+		'nowait'	=> { 'warning' => 95, 'error' => 90 },
+		'sort'		=> { 'warning' => 95, 'error' => 90 }
+	);
+our $db_name = 'TIGER';
+our $db_user = 'system';
+our $db_password = 'manager';
+our $env_nls_lang = 'AMERICAN_AMERICA.WE8ISO8859P9';
+our $env_oracle_home = '/opt/oracle/product/8.1.7';
+our $env_oracle_bin = '/opt/oracle/product/8.1.7/bin';
 #### end of the default values
 ################################################################################
 ## override defaults if there is a corresponfing conf file
@@ -48,60 +61,106 @@ my $statusid = $SisIYA_Config::statusids{'ok'};
 my $service_name = 'oracle_hitratios';
 my $error_str = '';
 my $ok_str = '';
-#my $warning_str = '';
-my @ps_list;
+my $warning_str = '';
 
-sub is_running
-{
-	my $prog = $_[0];
-	my $found = 0;
+### set environment variables
+$ENV{'ORACLE_HOME'} = $env_oracle_home;
+$ENV{'PATH'} = $env_oracle_bin.':'.$ENV{'PATH'};
+$ENV{'NLS_LANG'} = $env_nls_lang;
 
-	for my $i (0..$#ps_list) {
-		if($prog eq $ps_list[$i]) {
-			$found = 1;
-			last;
-		}
-	}
-	return $found;
+### bufer cache hit ratio
+my $sql_file = $SisIYA_Config::sisiya_special_dir.'/sisiya_oracle_hitratios_buffercache.sql';
+my $x;
+chomp($x= `$sqlplus_prog -S $db_user/$db_password\@$db_name \@$sql_file`);
+$x = trim($x); 
+print STDERR "x=$x\n";
+my $s = sprintf("%.2f", $x);
+if($x <= $hitratios{'buffer_cache'}{'error'}) {
+	$error_str = "ERROR: Buffer cache hit ratio is $s\% <= $hitratios{'buffer_cache'}{'error'}\%!";
+}
+elsif($x <= $hitratios{'buffer_cache'}{'warning'}) {
+	$warning_str = "WARNING: Buffer cache hit ratio is $s\% <= $hitratios{'buffer_cache'}{'warning'}\%!";
+}
+else {
+	$ok_str = "OK: Buffer cache hit ratio is $s\%.";
 }
 
-my $ps_params = '-eo comm';
-if($SisIYA_Config::sisiya_osname eq 'OpenBSD') {
-	$ps_params = '-xeo comm';
+### dictionary hit ratio
+$sql_file = $SisIYA_Config::sisiya_special_dir.'/sisiya_oracle_hitratios_dictionary.sql';
+chomp($x= `$sqlplus_prog -S $db_user/$db_password\@$db_name \@$sql_file`);
+$x = trim($x); 
+print STDERR "x=$x\n";
+$s = sprintf("%.2f", $x);
+if($x <= $hitratios{'dictionary'}{'error'}) {
+	$error_str .= " ERROR: Dictionary cache hit ratio is $s\% <= $hitratios{'dictionary'}{'error'}\%!";
 }
-#elsif($SisIYA_Config::sisiya_osname eq 'HP-UX') {
-#	# see if I need to UNIX95="" ; export UNIX95
-#	$ps_params = '-eo comm';
-#}
-#elsif($SisIYA_Config::sisiya_osname eq 'SunOS') {
-#	$ps_params = '-eo comm';
-#}
-elsif($SisIYA_Config::sisiya_osname eq 'Linux') {
-	$ps_params = '-eo command';
+elsif($x <= $hitratios{'dictionary'}{'warning'}) {
+	$warning_str .= " WARNING: Dictionary cache hit ratio is $s\% <= $hitratios{'dictionary'}{'warning'}\%!";
+}
+else {
+	$ok_str .= " OK: Dictionary cache hit ratio is $s\%.";
 }
 
-chomp(@ps_list = `$ps_prog $ps_params`);
-for my $i (0..$#progs) {
-	if(is_running($progs[$i])) {
-		$ok_str .= " $progs[$i]";
-	}
-	else {
-		$error_str .= " $progs[$i]";
-	}
+### library cache hit ratio
+$sql_file = $SisIYA_Config::sisiya_special_dir.'/sisiya_oracle_hitratios_library.sql';
+chomp($x= `$sqlplus_prog -S $db_user/$db_password\@$db_name \@$sql_file`);
+$x = trim($x); 
+print STDERR "x=$x\n";
+$s = sprintf("%.2f", $x);
+if($x <= $hitratios{'library'}{'error'}) {
+	$error_str .= " ERROR: Library cache hit ratio is $s\% <= $hitratios{'library'}{'error'}\%!";
+}
+elsif($x <= $hitratios{'library'}{'warning'}) {
+	$warning_str .= " WARNING: Library cache hit ratio is $s\% <= $hitratios{'library'}{'warning'}\%!";
+}
+else {
+	$ok_str .= " OK: Library cache hit ratio is $s\%.";
+}
+
+### nowait cache hit ratio
+$sql_file = $SisIYA_Config::sisiya_special_dir.'/sisiya_oracle_hitratios_nowait.sql';
+chomp($x= `$sqlplus_prog -S $db_user/$db_password\@$db_name \@$sql_file`);
+$x = trim($x); 
+print STDERR "x=$x\n";
+$s = sprintf("%.2f", $x);
+if($x <= $hitratios{'nowait'}{'error'}) {
+	$error_str .= " ERROR: Nowait hit ratio is $s\% <= $hitratios{'nowait'}{'error'}\%!";
+}
+elsif($x <= $hitratios{'nowait'}{'warning'}) {
+	$warning_str .= " WARNING: Nowait hit ratio is $s\% <= $hitratios{'nowait'}{'warning'}\%!";
+}
+else {
+	$ok_str .= " OK: Nowait hit ratio is $s\%.";
+}
+
+### sort cache hit ratio
+$sql_file = $SisIYA_Config::sisiya_special_dir.'/sisiya_oracle_hitratios_sort.sql';
+chomp($x= `$sqlplus_prog -S $db_user/$db_password\@$db_name \@$sql_file`);
+$x = trim($x); 
+print STDERR "x=$x\n";
+$s = sprintf("%.2f", $x);
+if($x <= $hitratios{'sort'}{'error'}) {
+	$error_str .= " ERROR: Sort hit ratio is $s\% <= $hitratios{'sort'}{'error'}\%!";
+}
+elsif($x <= $hitratios{'sort'}{'warning'}) {
+	$warning_str .= " WARNING: Sort hit ratio is $s\% <= $hitratios{'sort'}{'warning'}\%!";
+}
+else {
+	$ok_str .= " OK: Sort hit ratio is $s\%.";
 }
 
 if($error_str ne '') {
 	$statusid = $SisIYA_Config::statusids{'error'};
-	$message_str = "ERROR:$error_str";
+	$message_str = "$error_str";
 }
-#elsif($warning_str ne '') {
-#	if($statusid < $SisIYA_Config::statusids{'warning'}) {
-#		$statusid = $SisIYA_Config::statusids{'warning'};
-#	}	
-#	$message_str .= " WARNING:$warning_str";
-#}
+elsif($warning_str ne '') {
+	if($statusid < $SisIYA_Config::statusids{'warning'}) {
+		$statusid = $SisIYA_Config::statusids{'warning'};
+	}	
+	$message_str .= " $warning_str";
+}
 if($ok_str ne '') {
-	$message_str .= " OK:$ok_str";
+	$message_str .= " $ok_str";
 }
 ################################################################################
 #print "listening_socket$SisIYA_Config::FS<msg>$message_str</msg><datamsg></datamsg>\n";
