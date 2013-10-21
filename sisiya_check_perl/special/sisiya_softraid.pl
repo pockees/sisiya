82fec87cd4c4d9ad3d3f0226f21c06f2fa516fe0
Erdal Mutlu <erdal@sisiya.net>
2013-10-21 21:46:23 +0300

    rewriting check scripts in Perl

--- sisiya_check_perl/special/sisiya_softraid.pl
+++ sisiya_check_perl/special/sisiya_softraid.pl
@@ -48,9 +48,8 @@ sub get_array_list
 	my @b;
 	my $i = 0;
 	foreach(@a) {
-		@b = split(/\s+/, $_);
-		$a[$i] = $b[1];
-		print STDERR "$a[$i]\n";
+		$a[$i] = (split(/\s+/, $_))[1];
+		#print STDERR "$a[$i]\n";
 		$i++;
 	}
 	return @a;
@@ -65,8 +64,6 @@ my $warning_str = '';
 my @raid_arrays;
 my @a;
 
-$message_str = "deneme";
-
 @a = `$mdadm_prog --detail --scan`;
 my $retcode = $? >>=8;
 if($retcode != 0) {
@@ -75,19 +72,62 @@ if($retcode != 0) {
 	sisiya_exit($SisIYA_Config::FS, $service_name, $statusid, $message_str);
 }
 @raid_arrays = get_array_list(@a);
+my $state;
+my @b;
+my $raid_level;
+my $total_devs;
+my $raid_devs;
+my $working_devs;
+my $active_devs;
+my $failed_devs;
+my $spare_devs;
+my $rebuild_status;
+for my $i (0..$#raid_arrays) {
+	@a = `$mdadm_prog --detail $raid_arrays[$i]`;
+	$retcode = $? >>=8;
+	if($retcode != 0) {
+		$error_str .= " ERROR: Could not get info about $raid_arrays[$i]!";
+	}
+	else {
+		chomp(@a = @a);
+		$state = trim(( split(/:/, ( grep(/State :/, @a) )[0]) )[1]);
+		$raid_level = trim(( split(/:/, ( grep(/Raid Level :/, @a) )[0]) )[1]);
+		$raid_devs = trim(( split(/:/, ( grep(/Raid Devices :/, @a) )[0]) )[1]);
+		$total_devs = trim(( split(/:/, ( grep(/Total Devices :/, @a) )[0]) )[1]);
+		$active_devs = trim(( split(/:/, ( grep(/Active Devices :/, @a) )[0]) )[1]);
+		$working_devs = trim(( split(/:/, ( grep(/Working Devices :/, @a) )[0]) )[1]);
+		$failed_devs = trim(( split(/:/, ( grep(/Failed Devices :/, @a) )[0]) )[1]);
+		$spare_devs = trim(( split(/:/, ( grep(/Spare Devices :/, @a) )[0]) )[1]);
+		#print STDERR "$state\n";
+
+		if( ($state eq 'active') || ($state eq 'clean') ) {
+			$ok_str .= " OK: $raid_arrays[$i] RAID level $raid_level is $state (RAID devices=$raid_devs, total=$total_devs, active=$active_devs, warking=$working_devs, failde=$failed_devs, spare=$spare_devs).";
+		}
+		elsif($state eq 'active,checking') {
+			$warning_str .= " WARNING: $raid_arrays[$i] RAID level $raid_level is $state (RAID devices=$raid_devs, total=$total_devs, active=$active_devs, warking=$working_devs, failde=$failed_devs, spare=$spare_devs)!";
+		}
+		elsif( ($state eq 'active,resyncing') || ($state eq 'clean,resyncing') ) {
+			$rebuild_status = trim(( split(/:/, ( grep(/Rebuild status :/, @a) )[0]) )[1]);
+			$warning_str .= " WARNING: $raid_arrays[$i] RAID level $raid_level is $state (RAID devices=$raid_devs, total=$total_devs, active=$active_devs, warking=$working_devs, failde=$failed_devs, spare=$spare_devs) Rebuild status $rebuild_status.";
+		}
+		else {
+			$error_str .= " ERROR: $raid_arrays[$i] RAID level $raid_level is $state (RAID devices=$raid_devs, total=$total_devs, active=$active_devs, warking=$working_devs, failde=$failed_devs, spare=$spare_devs)!";
+		}
+	}
+}
 
 if($error_str ne '') {
 	$statusid = $SisIYA_Config::statusids{'error'};
-	$message_str = "ERROR:$error_str";
+	$message_str = "$error_str";
 }
 elsif($warning_str ne '') {
 	if($statusid < $SisIYA_Config::statusids{'warning'}) {
 		$statusid = $SisIYA_Config::statusids{'warning'};
 	}	
-	$message_str .= " WARNING:$warning_str";
+	$message_str .= "$warning_str";
 }
 if($ok_str ne '') {
-	$message_str .= " OK:$ok_str";
+	$message_str .= "$ok_str";
 }
 ################################################################################
 #print "listening_socket$SisIYA_Config::FS<msg>$message_str</msg><datamsg></datamsg>\n";
