6fb93a2fb0fefc7aa5e81b00b510886c90bfa648
Erdal Mutlu <erdal@sisiya.net>
2013-11-14 00:31:15 +0200

    rewriting check scripts in Perl

--- sisiya_check_perl/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#perl -I/ev/erdalmutlu/erdal/sisiya/sisiya/sisiya_check_perl bin/sisiya_all.pl /ev/erdalmutlu/erdal/sisiya/sisiya/SisIYA_Config.pm 10
-#
-perl -I/ev/erdalmutlu/erdal/sisiya/sisiya/sisiya_check_perl bin/sisiya_all.pl 10 
-#
-#perl -I/root/s bin/sisiya_all.pl 10 
