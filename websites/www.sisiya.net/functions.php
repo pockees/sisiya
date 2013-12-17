9fae4f4debfe8ad9391096ead1a601481335a11d
Erdal Mutlu <erdal@sisiya.net>
2013-12-17 22:36:26 +0200

    restructuring

--- websites/www.sisiya.net/functions.php
+++ websites/www.sisiya.net/functions.php
@@ -500,20 +500,4 @@ function eliminateSpecialCharacters($text) {
 	
 }
 
-
-function iptocountry($ip) {    
-    $numbers = preg_split( "/\./", $ip);    
-    include("ip_files/".$numbers[0].".php");
-    $code=($numbers[0] * 16777216) + ($numbers[1] * 65536) + ($numbers[2] * 256) + ($numbers[3]);    
-    foreach($ranges as $key => $value){
-        if($key<=$code){
-            if($ranges[$key][0]>=$code){$country=$ranges[$key][1];break;}
-            }
-    }
-    if ($country==""){$country="?";}
-    return $country;
-}
-
-
 ?>
-
