a06ce6a7405edf4adbe9dfef39dcc5d6313490f2
Erdal Mutlu <erdal@sisiya.net>
2014-11-16 23:15:13 +0200

    up

--- sisiya-remote-checks/scripts/sisiya_check_qnap.pl
+++ sisiya-remote-checks/scripts/sisiya_check_qnap.pl
@@ -53,15 +53,16 @@ if(-f $SisIYA_Remote_Config::functions) {
 # default values
 our %uptimes = ('error' => 1440, 'warning' => 4320);
 our %mibs = (
-	'system_hd_table'	=> 'SNMPv2-SMI::enterprises.24681.1.2.11',		# 
-	'hd_number'		=> 'SNMPv2-SMI::enterprises.24681.1.2.10.0',		# number of hard disks
-	'hd_index'		=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.1.',		# add the disk number to get the index for hd_status, hd_model, hd_capacity ...
-	'hd_status'		=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.4.',		# add the disk number at the end
-	'hd_descr'		=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.2.',		# add the disk index
-	'hd_capacity'		=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.6.',		# add the disk index
-	'hd_model'		=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.5.',		# add the disk index
-	'hd_temperature'	=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.3.1.',	# add the disk index
-	'hd_smart_info'		=> 'SNMPv2-SMI::enterprises.24681.1.2.11.1.7.',		# add the disk index
+	'storage'		=> '1.3.6.1.4.1.24681',			# whole storage tree
+	'system_hd_table'	=> '1.3.6.1.4.1.24681.1.2.11',		# whole hd tree
+	'hd_number'		=> '1.3.6.1.4.1.24681.1.2.10.0',	# number of hard disks
+	'hd_index'		=> '1.3.6.1.4.1.24681.1.2.11.1.1.',	# add the disk number to get the index for hd_status, hd_model, hd_capacity ...
+	'hd_status'		=> '1.3.6.1.4.1.24681.1.2.11.1.4.',	# add the disk number at the end
+	'hd_descr'		=> '1.3.6.1.4.1.24681.1.2.11.1.2.',	# add the disk index
+	'hd_capacity'		=> '1.3.6.1.4.1.24681.1.2.11.1.6.',	# add the disk index
+	'hd_model'		=> '1.3.6.1.4.1.24681.1.2.11.1.5.',	# add the disk index
+	'hd_temperature'	=> '1.3.6.1.4.1.24681.1.2.11.1.3.',	# add the disk index
+	'hd_smart_info'		=> '1.3.6.1.4.1.24681.1.2.11.1.7.',	# add the disk index
 );
 
 # end of default values
@@ -71,31 +72,26 @@ sub get_snmp_value_from_array
 	my ($search_str, @a) = @_;
 	
 	print STDERR "search str: $search_str\n";
-	return "W";
+	my $s = (grep(/$search_str/, @a))[0];
+	print STDERR "1 s=$s\n";
+	if ($s) {
+		$s = trim((split(/=/, $s))[1]);
+	}
+	print STDERR "2 s=$s\n";
+	return $s;
 }
 
 sub check_qnap_smart
 {
-	my ($expire, $hostname, $snmp_version, $community, $username, $password) = @_;
+	my ($expire, $system_name, $hostname, $snmp_version, $community, $username, $password) = @_;
 	my $error_str = '';
 	my $warning_str = '';
 	my $ok_str = '';
 	my $serviceid = get_serviceid('smart');
 	my $statusid = $SisIYA_Config::statusids{'ok'};
 
-	# get the system hd table in an array
-        #my @a = `$SisIYA_Remote_Config::external_progs{'snmpwalk'} -OvQ -v $snmp_version -c $community $hostname $mibs{'system_hd_table'} 2>&1`;
-        my @a = `$SisIYA_Remote_Config::external_progs{'snmpwalk'} -v $snmp_version -c $community $hostname $mibs{'system_hd_table'} 2>&1`;
-        my $retcode = $? >>=8;
-        if ($retcode != 0) {
-                return '';
-        }
-        #chomp(@a = @a);
-	print STDERR "@a\n";
-	my $s = get_snmp_value_from_array($mibs{'hd_number'}, @a);
-	print STDERR "s=$s\n";
-
-	$s = get_snmp_value('-OvQ', $hostname, $snmp_version, $community, $mibs{'hd_number'}, $username, $password);
+	#print STDERR "@a\n";
+	my $s = get_snmp_value('-OvQ', $hostname, $snmp_version, $community, $mibs{'hd_number'}, $username, $password);
 	if ($s eq '') {
 		return;
 	}
@@ -104,26 +100,38 @@ sub check_qnap_smart
 	if ($hd_number == 0) {
 		return;
 	}
-	my $index;
+	# get the system hd table in an array
+        my @a = `$SisIYA_Remote_Config::external_progs{'snmpwalk'} -OnQ -v $snmp_version -c $community $hostname $mibs{'system_hd_table'} 2>&1`;
+        my $retcode = $? >>=8;
+        if ($retcode != 0) {
+                return '';
+        }
+	my ($hd_smart_info, $hd_model, $hd_descr, $hd_capacity, $hd_status, $hd_temperature, $index);
 	for (my $i = 1; $i <= $hd_number; $i++) {
-		$s = get_snmp_value('-OvQ', $hostname, $snmp_version, $community, $mibs{'hd_index'}.$i, $username, $password);
-		if ($s eq '') {
-			return;
-		}
-		$index = $s;
-		print STDERR "index: $index\n";
-		$s = get_snmp_value('-OvQ', $hostname, $snmp_version, $community, $mibs{'hd_smart_info'}.$index, $username, $password);
-		if ($s eq '') {
-			return;
-		}
-		print STDERR "Disk: $index smart: $s\n";
-		if ($s eq '"GOOD"') {
-			$ok_str .= " OK: Disk $index is OK.";
+		$index = get_snmp_value_from_array($mibs{'hd_index'}.$i, @a);
+		$hd_capacity = get_snmp_value_from_array($mibs{'hd_capacity'}.$i, @a);
+		$hd_capacity =~ s/"//g;
+		$hd_descr = get_snmp_value_from_array($mibs{'hd_descr'}.$i, @a);
+		$hd_descr =~ s/"//g;
+		$hd_model = get_snmp_value_from_array($mibs{'hd_model'}.$i, @a);
+		$hd_model =~ s/"//g;
+		$hd_status = get_snmp_value_from_array($mibs{'hd_status'}.$i, @a);
+		$hd_temperature = get_snmp_value_from_array($mibs{'hd_temperature'}.$i, @a);
+		$hd_temperature =~ s/"//g;
+		$hd_temperature = (split(/\//, $hd_temperature))[0];
+		
+		$hd_smart_info = get_snmp_value_from_array($mibs{'hd_smart_info'}.$i, @a);
+		$hd_smart_info =~ s/"//g;
+		print STDERR "index: $index hd_status=[$hd_status] hd_capacity=[$hd_capacity] hd_model=[$hd_model] hd_descr=[$hd_descr] \n";
+		$s = "$hd_temperature $hd_descr Device Model: $hd_model Capacity: $hd_capacity status: $hd_status";
+		if ($hd_smart_info eq 'GOOD') {
+			$ok_str .= " OK: Disk $index is OK. $s";
 		} else {
 			$statusid = $SisIYA_Config::statusids{'error'};
-			$error_str .= " ERROR: Disk $index has status = $s (!= GOOD)!";
+			$error_str .= " ERROR: Disk $index has status = $hd_smart_info (!= GOOD)! $s";
 		}
 	}
+
 	return "<message><serviceid>$serviceid</serviceid><statusid>$statusid</statusid><expire>$expire</expire><data><msg>$error_str$warning_str$ok_str</msg><datamsg></datamsg></data></message>";
 }
 
@@ -140,7 +148,7 @@ sub check_qnap
 	if ($s eq '') {
 		return '';
 	}
-	$s .= check_qnap_smart($expire, $hostname, $snmp_version, $community, $username, $password);
+	$s .= check_qnap_smart($expire, $system_name, $hostname, $snmp_version, $community, $username, $password);
 	return "<system><name>$system_name</name>$s</system>";
 }
 
